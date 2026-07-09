a78afcdb534bc725f67af80d8f38135b1602697a
eduardaurbanovichi-png <eduardaurbanovichi@gmail.com>
2026-07-08 21:38:40 -0300

    Update api.js

---
+++ js/api.js
@@ -0,0 +1,8 @@
+const OpenRouterAPI = {
+    getSystemPrompt() {
+        // ... restante do código do prompt
+    },
+    async sendMessage(messagesHist) {
+        // ... restante do código do fetch
+    }
+};
